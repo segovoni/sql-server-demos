8cdd0152873edb11aa5d0e673077a5c5e7380ff2
Sergio Govoni <segovoni@gmail.com>
2023-06-13 18:05:09 +0200

    Add parallel plan demo script

---
+++ sqlstart/2023/sql-server-2022-degree-of-parallelism-feedback/demos/20-parallel-execution-plan.sql
@@ -0,0 +1,127 @@
+------------------------------------------------------------------------
+-- Event:    SQL Start 2023, June 16                                  --
+--           https://www.sqlstart.it/2023/Speakers/Sergio-Govoni      --
+--                                                                    --
+-- Session:  SQL Server 2022 Degree of Parallelism Feedback           --
+-- Demo:     Parallel Execution Plan                                  --
+-- Author:   Sergio Govoni                                            --
+-- Notes:    -                                                        --
+------------------------------------------------------------------------
+
+USE [AdventureWorks2019]
+GO
+
+-- Parallel plan for running total calculation
+SELECT
+  T.ProductID
+  ,T.TransactionDate
+  ,T.TransactionType
+  ,CASE (T.TransactionType)
+     WHEN 'S' THEN (T.Quantity * -1)
+     ELSE (T.Quantity)
+   END AS Quantity
+  ,SUM(
+         CASE (T1.TransactionType)
+           WHEN 'S' THEN (T1.Quantity * -1)
+		   ELSE (T1.Quantity)
+	     END
+	  ) AS StockLevel
+FROM
+  Production.TransactionHistory AS T
+JOIN
+  Production.TransactionHistory AS T1
+  ON (T.ProductID = T1.ProductID)
+     AND (T1.TransactionID <= T.TransactionID)
+GROUP BY
+  T.ProductID
+  ,T.TransactionDate
+  ,T.TransactionType
+  ,T.Quantity
+  ,T.TransactionID
+ORDER BY
+  T.ProductID
+  ,T.TransactionID;
+GO 6
+
+
+-- Parallel region due to TOP
+SELECT
+  T.ProductID
+  ,T.TransactionDate
+  ,T.TransactionType
+  ,CASE (T.TransactionType)
+     WHEN 'S' THEN (T.Quantity * -1)
+     ELSE (T.Quantity)
+   END AS Quantity
+  ,SUM(
+         CASE (T1.TransactionType)
+           WHEN 'S' THEN (T1.Quantity * -1)
+		   ELSE (T1.Quantity)
+	     END
+	  ) AS StockLevel
+FROM
+  (
+    SELECT TOP (2000000) *
+	FROM Production.TransactionHistory
+  ) T
+JOIN
+  Production.TransactionHistory AS T1
+  ON (T.ProductID = T1.ProductID)
+     AND (T1.TransactionID <= T.TransactionID)
+GROUP BY
+  T.ProductID
+  ,T.TransactionDate
+  ,T.TransactionType
+  ,T.Quantity
+  ,T.TransactionID
+ORDER BY
+  T.ProductID
+  ,T.TransactionID;
+GO
+
+
+-- Parallel region due to query global aggregate
+/*
+SELECT
+  SUM(X.Quantity) AS QuantitySUM
+FROM
+(
+SELECT
+  T.ProductID
+  ,T.TransactionDate
+  ,T.TransactionType
+  ,CASE (T.TransactionType)
+     WHEN 'S' THEN (T.Quantity * -1)
+     ELSE (T.Quantity)
+   END AS Quantity
+  ,SUM(
+         CASE (T1.TransactionType)
+           WHEN 'S' THEN (T1.Quantity * -1)
+		   ELSE (T1.Quantity)
+	     END
+	  ) AS StockLevel
+FROM
+  Production.TransactionHistory AS T
+JOIN
+  Production.TransactionHistory AS T1
+  ON (T.ProductID = T1.ProductID)
+     AND (T1.TransactionID <= T.TransactionID)
+GROUP BY
+  T.ProductID
+  ,T.TransactionDate
+  ,T.TransactionType
+  ,T.Quantity
+  ,T.TransactionID
+) X
+OPTION (QUERYTRACEON 8649);
+--OPTION(USE HINT('ENABLE_PARALLEL_PLAN_PREFERENCE'));
+GO
+*/
+
+
+USE [WideWorldImporters];
+GO
+
+-- Waiting task for CXPACKET
+EXEC Warehouse.GetStockItemsbySupplier 4;
+GO 15
