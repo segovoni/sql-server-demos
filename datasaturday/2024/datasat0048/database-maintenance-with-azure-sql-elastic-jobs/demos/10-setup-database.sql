fc6cb96a7c50467517ced01da9ab9208caf4cc06
Sergio Govoni <segovoni@gmail.com>
2023-12-31 17:35:34 +0100

    Initial commit demo script Data Saturday Pordenone 2024

---
+++ datasaturday/2024/datasat0048/database-maintenance-with-azure-sql-elastic-jobs/demos/10-setup-database.sql
@@ -0,0 +1,118 @@
+------------------------------------------------------------------------
+-- Event:        Data Saturday Pordenone 2024 - February 24           --
+--               https://bit.ly/3R8aAEM                               --
+--                                                                    --
+-- Session:      Database maintenance with Azure SQL Elastic Jobs     --
+--                                                                    --
+-- Demo:         Setup database                                       --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+/*
+Stack Overflow SQL Server Database - Mini 2010 Version
+
+For more information and the latest release:
+http://www.brentozar.com/go/querystack
+
+Imported from the Stack Exchange Data Dump circa 2010
+https://archive.org/details/stackexchange
+
+Imported using the Stack Overflow Data Dump Importer:
+https://github.com/BrentOzarULTD/soddi
+
+This database is in Microsoft SQL Server 2008 format, which means you can
+attach it to any SQL Server 2008 or newer instance.
+
+To keep the size small but let you get started fast:
+
+* All tables have a clustered index
+* No nonclustered or full text indexes are included
+* The log file is small, and you should grow it out if you plan to modify data
+* It's distributed as an mdf/ldf so you don't need space to restore it
+* It only includes StackOverflow.com data, not data for other Stack sites
+
+As with the original data dump, this is provided under cc-by-sa 3.0 license:
+http://creativecommons.org/licenses/by-sa/3.0/
+
+You are free to share this database and adapt it for any purpose, even
+commercially, but you must attribute it to the original authors:
+https://archive.org/details/stackexchange
+*/
+
+
+USE [master];
+GO
+
+-- StackOverflow2010
+IF (DB_ID('StackOverflow2010') IS NOT NULL)
+BEGIN
+  ALTER DATABASE [StackOverflow2010]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+  DROP DATABASE [StackOverflow2010];
+END;
+GO
+
+RESTORE DATABASE [StackOverflow2010]
+  FROM DISK = N'C:\SQL\DBs\Backup\StackOverflow2010.bak'
+  WITH
+    FILE = 1
+    ,MOVE N'StackOverflow2010' TO N'C:\SQL\DBs\StackOverflow2010.mdf'
+    ,MOVE N'StackOverflow2010_log' TO N'C:\SQL\DBs\StackOverflow2010_log.ldf'
+    ,NOUNLOAD
+    ,STATS = 5;
+GO
+
+-- COMPATIBILITY_LEVEL { 160 | 150 | 140 | 130 | 120 | 110 | 100 | 90 | 80 }
+-- 130 for SQL Server 2016
+-- 140 for SQL Server 2017
+-- 150 for SQL Server 2019
+-- 160 for SQL Server 2022
+ALTER DATABASE [StackOverflow2010] SET COMPATIBILITY_LEVEL = 160;
+GO
+
+
+-- Connect to Azure SQL Database
+
+-- DBJobsDataSaturdayPordenone2024
+IF (DB_ID('DBJobsDataSaturdayPordenone2024') IS NOT NULL)
+BEGIN
+  ALTER DATABASE [DBJobsDataSaturdayPordenone2024]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE [DBJobsDataSaturdayPordenone2024];
+END
+GO
+
+CREATE DATABASE [DBJobsDataSaturdayPordenone2024];
+GO
+
+
+SELECT * FROM sys.database_service_objectives;
+GO
+
+-- Change the pricing tier
+-- The edition is the tier like Basic, Standard, Premium
+-- The Basic edition has only Basic as a service object
+-- In the Standard edition, we have S0 to S12 service objectives
+-- For the Premium tier, you have P1 to P15 service objects
+ALTER DATABASE [DBJobsDataSaturdayPordenone2024]
+  MODIFY(EDITION = 'Standard', SERVICE_OBJECTIVE = 'S1');
+GO
+
+
+-- DBDemo
+IF (DB_ID('DBDemo') IS NOT NULL)
+BEGIN
+  ALTER DATABASE [DBDemo]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE [DBDemo];
+END
+GO
+
+CREATE DATABASE [DBDemo];
+GO
+
+ALTER DATABASE [DBDemo]
+  MODIFY(EDITION = 'Basic');
+GO
---
+++ datasaturday/2024/datasat0048/database-maintenance-with-azure-sql-elastic-jobs/demos/30-configure-elastic-jobs-umi-authentication.sql
@@ -0,0 +1,184 @@
+------------------------------------------------------------------------
+-- Event:        Data Saturday Pordenone 2024 - February 24           --
+--               https://bit.ly/3R8aAEM                               --
+--                                                                    --
+-- Session:      Database maintenance with Azure SQL Elastic Jobs     --
+--                                                                    --
+-- Demo:         Configure elastic jobs (UMI Authentication)          --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+ -- Create a target group and add targets for the jobs
+ -- Define the target group and targets (the databases you want to 
+ -- run the job against)
+
+-- https://learn.microsoft.com/azure/azure-sql/database/elastic-jobs-tsql-create-manage?view=azuresql#define-target-servers-and-databases
+
+-- Connect to the job database specified when creating the job agent
+
+USE [DBJobsDataSaturdayPordenone2024];
+GO
+
+
+-- Add a target group containing server(s)
+EXEC jobs.sp_add_target_group 'ServerGroupDemo';
+GO
+
+
+-- Add a server target member
+-- When using Microsoft Entra authentication,
+-- omit the @refresh_credential_name parameter, which should only be provided
+-- when using database-scoped credentials
+EXEC jobs.sp_add_target_group_member
+  @target_group_name = N'ServerGroupDemo'
+  ,@target_type = N'SqlServer'
+  -- Credential required to refresh the databases in server
+  --,@refresh_credential_name = N'masterjobcredential'
+  ,@server_name = N'azure-sql-data-sat-pn-2024.database.windows.net';
+GO
+
+
+-- Exclude a database target member from the server target group
+/*
+EXEC jobs.sp_add_target_group_member
+  @target_group_name = N'ServerGroupDemo'
+  ,@membership_type = N'Exclude'
+  ,@target_type = N'SqlDatabase'
+  ,@server_name = N'azure-automation.database.windows.net'
+  ,@database_name = N'DBJobs';
+GO
+*/
+
+
+EXEC jobs.sp_add_target_group_member
+  @target_group_name = N'ServerGroupDemo'
+  ,@membership_type = N'Exclude'
+  ,@target_type = N'SqlDatabase'
+  ,@server_name = N'azure-sql-data-sat-pn-2024.database.windows.net'
+  ,@database_name = N'DBJobsDataSaturdayPordenone2024';
+GO
+
+/*
+EXEC jobs.sp_add_target_group_member
+  @target_group_name = N'ServerGroupDemo'
+  ,@membership_type = N'Exclude'
+  ,@target_type = N'SqlDatabase'
+  ,@server_name = N'azure-automation.database.windows.net'
+  ,@database_name = N'sp_alter_column_devtest';
+GO
+*/
+
+/*
+EXEC jobs.sp_add_target_group_member
+  @target_group_name = N'ServerGroupDemo'
+  ,@membership_type = N'Exclude'
+  ,@target_type = N'SqlDatabase'
+  ,@server_name = N'azure-automation.database.windows.net'
+  ,@database_name = N'ToBeMaintained';
+GO
+*/
+
+
+-- View the recently created target group and target group members
+SELECT * FROM jobs.target_groups WHERE target_group_name = 'ServerGroupDemo';
+SELECT * FROM jobs.target_group_members WHERE target_group_name = 'ServerGroupDemo';
+GO
+
+
+-- Add job for rebuild/reorganize indexes
+EXEC jobs.sp_add_job
+  @job_name='IndexOptimize'
+  ,@description='Rebuild or reorganize all indexes with fragmentation';
+GO
+
+
+-- Add job step for create table
+EXEC jobs.sp_add_jobstep
+  @job_name='IndexOptimize'
+  ,@command=N'EXECUTE dbo.IndexOptimize @Databases = ''DBDemo'', @FragmentationLow = NULL, @FragmentationMedium = ''INDEX_REORGANIZE,INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE'', @FragmentationHigh = ''INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE'', @FragmentationLevel1 = 5, @FragmentationLevel2 = 30'
+  --,@credential_name='jobcredential'
+  ,@target_group_name='ServerGroupDemo';
+GO
+
+
+EXEC jobs.sp_update_jobstep
+  @job_name='IndexOptimize'
+  ,@step_id = 1
+  ,@command=N'EXECUTE dbo.IndexOptimize @Databases = ''StackOverflow2010'', @FragmentationLow = ''INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE'', @FragmentationMedium = ''INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE'', @FragmentationHigh = ''INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE'', @FragmentationLevel1 = 5, @FragmentationLevel2 = 30, @UpdateStatistics = ''COLUMNS'', @OnlyModifiedStatistics = ''Y'''
+  --,@credential_name='jobcredential'
+  ,@target_group_name='ServerGroupDemo';
+GO
+
+
+SELECT * FROM jobs.jobs;
+SELECT * FROM jobs.jobsteps;
+SELECT * FROM jobs.target_groups WHERE target_group_name = 'ServerGroupDemo';
+SELECT * FROM jobs.target_group_members WHERE target_group_name = 'ServerGroupDemo';
+GO
+
+/*
+-- Drop job
+EXEC jobs.sp_delete_job
+  @job_name='IndexOptimize';
+GO
+*/
+
+
+-- There are two options for authentication of an elastic job agent to targets:
+
+-- The the recommended method: use database users mapped to user-assigned
+-- managed identity (UMI) to authenticate to target server(s)/database(s)
+
+-- The previous method: use database users mapped to database-scoped
+-- credentials in each database. Previously, database-scoped credentials were the
+-- only option for the elastic job agent to authenticate to targets
+
+
+-- In each of the target server(s)/database(s), create a contained user mapped to the UMI
+-- or database-scoped credential, using T-SQL or PowerShell
+
+-- https://learn.microsoft.com/azure/azure-sql/database/elastic-jobs-tsql-create-manage?view=azuresql#create-the-job-authentication
+
+-- https://learn.microsoft.com/azure/azure-sql/database/elastic-jobs-tutorial?view=azuresql#create-job-agent-authentication
+
+
+-- Connect to the master database of the Azure SQL logical instance of job agent
+-- Use universal with MFA authentication type
+
+-- Create a login on the master database mapped to a user-assigned managed identity (UMI)
+CREATE LOGIN [umi-data-sat-pn-2024] FROM EXTERNAL PROVIDER;
+GO
+
+-- Create a user on the master database mapped to a login
+-- It prevents the error:
+
+-- Failed to determine members of SqlServerTarget (server name 'azure-automation.database.windows.net', server location 'azure-automation.database.windows.net'): 
+-- The server principal "926e2a2b...@1f36c249..." is not able to access the database "master" under the current security context. 
+-- Cannot open database "master" requested by the login. The login failed.  Login failed for user '926e2a2b-b428-40fc-aa68-f891efbd644b@1f36c249-867f-46da-aa43-70a6ded1401e.'.
+
+-- ID Client 926e2a2b...
+
+CREATE USER [umi-data-sat-pn-2024] FROM EXTERNAL PROVIDER;
+GO
+
+-- Verificare perchè ho anche dati i diritti di collaboratore su tutte
+-- le risorse del resource group.. forse si può anche togliere
+
+
+USE [StackOverflow2010];
+GO
+
+-- Create a user on a user database mapped to a login
+CREATE USER [umi-data-sat-pn-2024] FROM EXTERNAL PROVIDER;
+GO
+
+
+-- Grant permissions as necessary to execute your jobs. For example, ALTER and CREATE TABLE:
+GRANT EXECUTE ON OBJECT::[dbo].[IndexOptimize] TO [umi-data-sat-pn-2024];
+GRANT EXECUTE ON OBJECT::[dbo].[CommandExecute] TO [umi-data-sat-pn-2024];
+
+--GRANT ALTER ON SCHEMA::dbo TO [UMI-Data-Saturday-Pordenone-2024];
+GO
+
+
