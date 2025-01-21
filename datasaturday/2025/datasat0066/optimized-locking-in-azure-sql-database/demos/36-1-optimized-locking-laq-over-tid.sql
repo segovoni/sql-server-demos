b8f1218a0735051bff912d9943d9140eb241aece
Sergio Govoni <segovoni@gmail.com>
2025-01-21 23:16:32 +0100

    Initial commit of demo scripts for LAQ over TID

---
+++ datasaturday/2025/datasat0066/optimized-locking-in-azure-sql-database/demos/36-1-optimized-locking-laq-over-tid.sql
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------
+-- Event:        Data Saturday Pordenone 2025, Feb 22                 --
+--               https://bit.ly/datasatpordenone25                    --
+--                                                                    --
+-- Session:      Optimized Locking in Azure SQL Database:             --
+--               Concurrency and performance at the next level!       --
+--                                                                    --
+-- Demo:         Lock after qualification over Transaction ID locking --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+USE [OptimizedLocking];
+GO
+
+
+DROP TABLE IF EXISTS dbo.TableC;
+
+CREATE TABLE dbo.TableC
+(
+  ID INTEGER NOT NULL,
+  CounterValue INTEGER NULL
+);
+
+INSERT INTO dbo.TableC VALUES (1, 10), (2, 20), (3, 30);
+GO
+
+
+/* Session 1 */
+BEGIN TRANSACTION;
+
+UPDATE
+  dbo.TableC
+SET
+  CounterValue = CounterValue + 10
+WHERE
+  ID = 1;
+
+SELECT
+  *
+FROM
+  sys.dm_tran_locks
+WHERE
+  request_session_id IN (53, @@SPID)
+  AND resource_type IN ('PAGE','RID','KEY','XACT');
+
+ROLLBACK;
---
+++ datasaturday/2025/datasat0066/optimized-locking-in-azure-sql-database/demos/36-2-optimized-locking-laq-over-tid.sql
@@ -0,0 +1,27 @@
+------------------------------------------------------------------------
+-- Event:        Data Saturday Pordenone 2025, Feb 22                 --
+--               https://bit.ly/datasatpordenone25                    --
+--                                                                    --
+-- Session:      Optimized Locking in Azure SQL Database:             --
+--               Concurrency and performance at the next level!       --
+--                                                                    --
+-- Demo:         Lock after qualification over Transaction ID locking --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+USE [OptimizedLocking];
+GO
+
+
+/* Session 2 */
+BEGIN TRANSACTION;
+
+UPDATE
+  dbo.TableC
+SET
+  CounterValue = CounterValue + 10
+WHERE
+  ID = 1;
+
+ROLLBACK;
