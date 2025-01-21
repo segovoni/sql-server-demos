5d44e917938cb78db229c2bab4896e61df03353b
Sergio Govoni <segovoni@gmail.com>
2025-01-21 23:02:37 +0100

    Initial commit of demo script for TID on-premise

---
+++ datasaturday/2025/datasat0066/optimized-locking-in-azure-sql-database/demos/30-1-optimized-locking-and-tid-on-premise.sql
@@ -0,0 +1,105 @@
+------------------------------------------------------------------------
+-- Event:        Data Saturday Pordenone 2025, Feb 22                 --
+--               https://bit.ly/datasatpordenone25                    --
+--                                                                    --
+-- Session:      Optimized Locking in Azure SQL Database:             --
+--               Concurrency and performance at the next level!       --
+--                                                                    --
+-- Demo:         Optimized locking and transaction ID (TID) locking   --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+-- On-premises SQL Server 2022
+USE [OptimizedLocking];
+GO
+
+/*
+ALTER DATABASE CURRENT SET ACCELERATED_DATABASE_RECOVERY = ON;
+ALTER DATABASE CURRENT SET READ_COMMITTED_SNAPSHOT ON;
+ALTER DATABASE CURRENT SET OPTIMIZED_LOCKING = OFF;
+GO
+*/
+
+-- A complete view
+SELECT
+  IsOptimizedLockingOn = DATABASEPROPERTYEX(DB_NAME(), 'IsOptimizedLockingOn')
+  ,RCSI = is_read_committed_snapshot_on
+  ,ADR  = is_accelerated_database_recovery_on
+ FROM
+   sys.databases
+ WHERE
+   (name = DB_NAME());
+GO
+
+
+-- Enable trace flags 3604 and 1200 (globally)
+
+-- Trace flag 3604 prints output to the console
+DBCC TRACEON(3604)
+-- Trace flag 1200 prints detailed lock information
+DBCC TRACEON(1200, -1);
+GO
+
+DBCC TRACESTATUS;
+GO
+DBCC TRACEOFF(1200, -1);
+GO
+
+
+DROP TABLE IF EXISTS dbo.TableA;
+
+CREATE TABLE dbo.TableA
+(
+  ColumnA INTEGER PRIMARY KEY NOT NULL,
+  ColumnB INTEGER NOT NULL
+);
+
+INSERT INTO dbo.TableA VALUES (1, 10),(2, 20),(3, 30);
+GO
+
+
+-- Inspect page ID with sys.fn_PhysLocFormatter and DBCC PAGE
+SELECT
+  *
+  ,PageId = sys.fn_PhysLocFormatter(%%physloc%%)
+FROM
+  dbo.TableA
+GO
+
+/*
+DBCC PAGE ( {'dbname' | dbid}, filenum, pagenum [, printopt={0|1|2|3} ])
+*/
+-- (1:2584:2)
+DBCC PAGE ('OptimizedLocking', 1, 2584, 3);
+GO
+
+/*
+(8194443284a0)                                                                                                                                                                                                                                                  
+(61a06abd401c)                                                                                                                                                                                                                                                  
+(98ec012aa510)                                                                                                                                                                                                                                                  
+*/
+
+
+-- Inspect locks with sys.dm_tran_locks on updated rows
+BEGIN TRANSACTION;
+
+UPDATE
+  dbo.TableA
+SET
+  ColumnB = ColumnB + 10;
+
+SELECT
+  *
+FROM
+  sys.dm_tran_locks
+WHERE
+  request_session_id = @@SPID
+AND
+  resource_type IN ('PAGE','RID','KEY','XACT');
+
+COMMIT TRANSACTION;
+GO
+
+DROP TABLE IF EXISTS dbo.TableA;
+GO
