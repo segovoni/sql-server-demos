46bddd26ec4c0f7e2fd5b03e5f41017e01ac9d4d
Sergio Govoni <segovoni@gmail.com>
2026-08-11 17:48:03 +0200

    Add script to enable external REST endpoint for SQL Server AI features

---
+++ 1nn0vai/2026/sql-server-2025-vector-search-embedding-and-ai-for-enterprise-data/demos/10-setup-sql-options.sql
@@ -0,0 +1,29 @@
+------------------------------------------------------------------------
+-- Event:        1nn0vAI 2026 - Pordenone, September  26              --
+--               https://www.1nn0vai.it/                              --
+--                                                                    --
+-- Session:      SQL Server 2025: Vector Search, Embedding, and AI    --
+--               for Enterprise Data                                  --
+--                                                                    --
+-- Demo:         Setup SQL Server options                             --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+USE [master];
+GO
+
+
+-- In order to call external REST endpoint, we must enable the
+-- 'external rest endpoint enabled' server configuration option
+EXEC sp_configure 'external rest endpoint enabled', 1;
+GO
+RECONFIGURE WITH OVERRIDE;
+GO
+
+EXEC sp_configure 'external rest endpoint enabled';
+GO
+
+
+-- Full-Text Search
+-- Evaluate if it is needed for a search comparison with vector search
