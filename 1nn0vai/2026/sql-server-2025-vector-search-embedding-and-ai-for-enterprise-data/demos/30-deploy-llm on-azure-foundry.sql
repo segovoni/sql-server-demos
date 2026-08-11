33e3a87a75a6ddc2c622152cc718c1302bbdbc9d
Sergio Govoni <segovoni@gmail.com>
2026-08-11 17:56:34 +0200

    Add info to deploy an LLM model on Azure OpenAI for embeddings

---
+++ 1nn0vai/2026/sql-server-2025-vector-search-embedding-and-ai-for-enterprise-data/demos/30-deploy-llm on-azure-foundry.sql
@@ -0,0 +1,14 @@
+------------------------------------------------------------------------
+-- Event:        1nn0vAI 2026 - Pordenone, September  26              --
+--               https://www.1nn0vai.it/                              --
+--                                                                    --
+-- Session:      SQL Server 2025: Vector Search, Embedding, and AI    --
+--               for Enterprise Data                                  --
+--                                                                    --
+-- Demo:         Deploy an LLM model on Azure OpenAI for embeddings   --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+
+-- https://learn.microsoft.com/azure/foundry/openai/tutorials/embeddings
