ca1e288a8f969e3c80447bbd9b566cc399394291
Sergio Govoni <segovoni@gmail.com>
2026-08-13 23:34:23 +0200

    Update session title in script headers

--- 1nn0vai/2026/sql-server-2025-vector-search-embedding-and-ai-for-enterprise-data/demos/20-setup-database.sql
+++ 1nn0vai/2026/sql-server-2025-vector-search-embedding-and-ai-for-enterprise-data/demos/20-setup-database.sql
@@ -2,8 +2,8 @@
 -- Event:        1nn0vAI 2026 - Pordenone, September  26              --
 --               https://www.1nn0vai.it/                              --
 --                                                                    --
--- Session:      SQL Server 2025: Vector Search, Embedding, and AI    --
---               for Enterprise Data                                  --
+-- Session:      SQL Server 2025 and Azure SQL: AI comes where        --
+--               business data lives                                  --
 --                                                                    --
 -- Demo:         Setup database                                       --
 -- Author:       Sergio Govoni                                        --
